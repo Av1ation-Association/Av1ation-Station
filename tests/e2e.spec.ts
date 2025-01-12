bdbf142a273b8a62ceab4cd7ad9c208432ccb628
Boats McGee <{ID}+{username}@users.noreply.github.com>
2025-01-11 19:26:34 -0500

    Remove template test

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -8,92 +8,92 @@ import { type BrowserWindow } from 'electron';
 import { globSync } from 'glob';
 import { platform } from 'node:process';
 
-process.env.PLAYWRIGHT_TEST = 'true';
+// process.env.PLAYWRIGHT_TEST = 'true';
 
-// Declare the types of your fixtures.
-type TestFixtures = {
-  electronApp: ElectronApplication;
-  electronVersions: NodeJS.ProcessVersions;
-};
+// // Declare the types of your fixtures.
+// type TestFixtures = {
+//   electronApp: ElectronApplication;
+//   electronVersions: NodeJS.ProcessVersions;
+// };
 
-const test = base.extend<TestFixtures>({
-    electronApp: [async ({}, use) => {
+// const test = base.extend<TestFixtures>({
+//     electronApp: [async ({}, use) => {
 
-        /**
-     * Executable path depends on root package name!
-     */
-        let executablePattern = 'dist/*/av1ation-station{,.*}';
-        if (platform === 'darwin') {
-            executablePattern += '/Contents/*/av1ation-station';
-        }
+//         /**
+//      * Executable path depends on root package name!
+//      */
+//         let executablePattern = 'dist/*/av1ation-station{,.*}';
+//         if (platform === 'darwin') {
+//             executablePattern += '/Contents/*/av1ation-station';
+//         }
 
-        const [executablePath] = globSync(executablePattern);
-        if (!executablePath) {
-            throw new Error('App Executable path not found');
-        }
+//         const [executablePath] = globSync(executablePattern);
+//         if (!executablePath) {
+//             throw new Error('App Executable path not found');
+//         }
 
-        const electronApp = await electron.launch({
-            executablePath: executablePath,
-        });
+//         const electronApp = await electron.launch({
+//             executablePath: executablePath,
+//         });
 
-        electronApp.on('console', (msg) => {
-            if (msg.type() === 'error') {
-                console.error(`[electron][${msg.type()}] ${msg.text()}`);
-            }
-        });
+//         electronApp.on('console', (msg) => {
+//             if (msg.type() === 'error') {
+//                 console.error(`[electron][${msg.type()}] ${msg.text()}`);
+//             }
+//         });
 
-        await use(electronApp);
+//         await use(electronApp);
 
-        // This code runs after all the tests in the worker process.
-        await electronApp.close();
-    }, {scope: 'worker', auto: true} as any],
+//         // This code runs after all the tests in the worker process.
+//         await electronApp.close();
+//     }, {scope: 'worker', auto: true} as any],
 
-    page: async ({electronApp}, use) => {
-        const page = await electronApp.firstWindow();
-        // capture errors
-        page.on('pageerror', (error) => {
-            console.error(error);
-        });
-        // capture console messages
-        page.on('console', (msg) => {
-            console.log(msg.text());
-        });
+//     page: async ({electronApp}, use) => {
+//         const page = await electronApp.firstWindow();
+//         // capture errors
+//         page.on('pageerror', (error) => {
+//             console.error(error);
+//         });
+//         // capture console messages
+//         page.on('console', (msg) => {
+//             console.log(msg.text());
+//         });
 
-        await page.waitForLoadState('load');
-        await use(page);
-    },
+//         await page.waitForLoadState('load');
+//         await use(page);
+//     },
 
-    electronVersions: async ({electronApp}, use) => {
-        await use(await electronApp.evaluate(() => process.versions));
-    },
-});
+//     electronVersions: async ({electronApp}, use) => {
+//         await use(await electronApp.evaluate(() => process.versions));
+//     },
+// });
 
 
-test('Main window state', async ({electronApp, page}) => {
-    const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
-    const windowState = await window.evaluate(
-        (mainWindow): Promise<{isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean}> => {
-            const getState = () => ({
-                isVisible: mainWindow.isVisible(),
-                isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
-                isCrashed: mainWindow.webContents.isCrashed(),
-            });
+// test('Main window state', async ({electronApp, page}) => {
+//     const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
+//     const windowState = await window.evaluate(
+//         (mainWindow): Promise<{isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean}> => {
+//             const getState = () => ({
+//                 isVisible: mainWindow.isVisible(),
+//                 isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
+//                 isCrashed: mainWindow.webContents.isCrashed(),
+//             });
 
-            return new Promise(resolve => {
-                /**
-         * The main window is created hidden, and is shown only when it is ready.
-         * See {@link ../packages/main/src/mainWindow.ts} function
-         */
-                if (mainWindow.isVisible()) {
-                    resolve(getState());
-                } else {
-                    mainWindow.once('ready-to-show', () => resolve(getState()));
-                }
-            });
-        },
-    );
+//             return new Promise(resolve => {
+//                 /**
+//          * The main window is created hidden, and is shown only when it is ready.
+//          * See {@link ../packages/main/src/mainWindow.ts} function
+//          */
+//                 if (mainWindow.isVisible()) {
+//                     resolve(getState());
+//                 } else {
+//                     mainWindow.once('ready-to-show', () => resolve(getState()));
+//                 }
+//             });
+//         },
+//     );
 
-    expect(windowState.isCrashed, 'The app has crashed').toEqual(false);
-    expect(windowState.isVisible, 'The main window was not visible').toEqual(true);
-    expect(windowState.isDevToolsOpened, 'The DevTools panel was open').toEqual(false);
-});
+//     expect(windowState.isCrashed, 'The app has crashed').toEqual(false);
+//     expect(windowState.isVisible, 'The main window was not visible').toEqual(true);
+//     expect(windowState.isDevToolsOpened, 'The DevTools panel was open').toEqual(false);
+// });
